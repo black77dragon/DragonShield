8e1d525b1d67d04d4ba4ec5589460f0e7a3a16f1
Black Dragon <rene.w.keller@gmail.com>
2025-07-24 06:41:48 +0800

    fix(ui): update onChange syntax in Top10PositionsTile

--- DragonShield/Views/DashboardTiles/DashboardTiles.swift
+++ DragonShield/Views/DashboardTiles/DashboardTiles.swift
@@ -139,57 +139,6 @@ struct TotalValueTile: DashboardTile {
     }
 }
 
-struct TopPositionsTile: DashboardTile {
-    @EnvironmentObject var dbManager: DatabaseManager
-    @StateObject private var viewModel = PositionsViewModel()
-
-    init() {}
-    static let tileID = "top_positions"
-    static let tileName = "Top 10 Positions by Asset Value (CHF)"
-    static let iconName = "list.number"
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 12) {
-            Text(Self.tileName)
-                .font(.headline)
-            if viewModel.calculating {
-                ProgressView()
-                    .frame(maxWidth: .infinity, alignment: .center)
-            } else {
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 4) {
-                        ForEach(Array(viewModel.top10PositionsCHF.enumerated()), id: \.element.id) { index, item in
-                            HStack {
-                                VStack(alignment: .leading, spacing: 2) {
-                                    Text(item.instrument)
-                                        .fontWeight(.semibold)
-                                        .lineLimit(1)
-                                    Text(String(format: "%.2f CHF", item.valueCHF))
-                                        .font(.caption)
-                                        .foregroundColor(Color(red: 30/255, green: 58/255, blue: 138/255))
-                                }
-                                Spacer()
-                                Text(item.currency)
-                                    .font(.caption)
-                                    .foregroundColor(.secondary)
-                            }
-                            .padding(4)
-                            .background(index == 0 ? Color(red: 191/255, green: 219/255, blue: 254/255) : Color.clear)
-                            .cornerRadius(6)
-                        }
-                    }
-                }
-                .frame(maxHeight: viewModel.top10PositionsCHF.count > 6 ? 220 : .infinity)
-            }
-        }
-        .padding(16)
-        .background(Color(red: 216/255, green: 236/255, blue: 248/255))
-        .cornerRadius(12)
-        .shadow(color: .black.opacity(0.1), radius: 3, x: 0, y: 2)
-        .onAppear { viewModel.calculateTop10Positions(db: dbManager) }
-        .accessibilityElement(children: .combine)
-    }
-}
 
 struct TextTile: DashboardTile {
     init() {}
@@ -258,7 +207,7 @@ enum TileRegistry {
         TileInfo(id: ListTile.tileID, name: ListTile.tileName, icon: ListTile.iconName) { AnyView(ListTile()) },
         TileInfo(id: MetricTile.tileID, name: MetricTile.tileName, icon: MetricTile.iconName) { AnyView(MetricTile()) },
         TileInfo(id: TotalValueTile.tileID, name: TotalValueTile.tileName, icon: TotalValueTile.iconName) { AnyView(TotalValueTile()) },
-        TileInfo(id: TopPositionsTile.tileID, name: TopPositionsTile.tileName, icon: TopPositionsTile.iconName) { AnyView(TopPositionsTile()) },
+        TileInfo(id: Top10PositionsTile.tileID, name: Top10PositionsTile.tileName, icon: Top10PositionsTile.iconName) { AnyView(Top10PositionsTile()) },
         TileInfo(id: CryptoTop5Tile.tileID, name: CryptoTop5Tile.tileName, icon: CryptoTop5Tile.iconName) { AnyView(CryptoTop5Tile()) },
 
         TileInfo(id: CurrencyExposureTile.tileID, name: CurrencyExposureTile.tileName, icon: CurrencyExposureTile.iconName) { AnyView(CurrencyExposureTile()) },
